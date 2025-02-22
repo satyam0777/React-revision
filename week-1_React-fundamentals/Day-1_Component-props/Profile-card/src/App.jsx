6afc1a532d4885be0bfae99bf5ecc74491af2573
satyam0777 <officialsatyam0777@gmail.com>
2025-02-22 15:43:58 +0530

    make profile card and try to use tailwind

--- week-1_React-fundamentals/Day-1_Component-props/Profile-card/src/App.jsx
+++ week-1_React-fundamentals/Day-1_Component-props/Profile-card/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ProfileCard from "./components/profilecard";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-700 p-5">
+      <h1 className="text-4x1 text-red font-bold mb-10">Team Profiles</h1>
 
-export default App
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <ProfileCard
+          name="Satyam Prajapati"
+          age={22}
+          image="https://framerusercontent.com/images/ufyPDeKNS6YYBL0nV3ofalgI.png"
+          isOnline={true}
+          bio="Full Stack Developer | Passionate about Web Technologies"
+        />
+        <ProfileCard
+          name="O P bro"
+          age={25}
+          image="https://framerusercontent.com/images/ufyPDeKNS6YYBL0nV3ofalgI.png"
+          isOnline={false}
+          bio="Open Source Contributor | Loves Python & React"
+        />
+        <ProfileCard
+          name="shubham"
+          age={28}
+          image="https://framerusercontent.com/images/ufyPDeKNS6YYBL0nV3ofalgI.png"
+          isOnline={true}
+          bio="UI/UX Designer | Creating Stunning Interfaces"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
